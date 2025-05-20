e61ce143bd86fd8da94497ecb2ddc9d57b6f6e43
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-05-20 02:59:30 +0100

    feat: add Astro configuration file for site setup and plugin integration

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import cloudflare from "@astrojs/cloudflare";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import { baseLocale, locales } from "./src/paraglide/runtime";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://massacritica.pt",
+  vite: {
+    plugins: [
+      tailwindcss(),
+      paraglideVitePlugin({
+        project: "./project.inlang",
+        outdir: "./src/paraglide",
+        disableAsyncLocalStorage: true,
+        strategy: ["url", "cookie"],
+      }),
+    ],
+  },
+  output: "static",
+  adapter: cloudflare(),
+  i18n: {
+    locales: [...locales],
+    defaultLocale: baseLocale,
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+});
