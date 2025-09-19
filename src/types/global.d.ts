526d3597a392fbe89fcff03602fddbd18c6e183a
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2025-09-19 15:59:38 +0100

    fix: update openImageModal types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,11 @@
 declare global {
   interface Window {
-    openImageModal: (imageSrc: string, imageAlt: string) => void;
+    openImageModal: (
+      imageSrc: string,
+      imageAlt: string,
+      authorName: string,
+      authorLink: string,
+    ) => void;
     filterByTag: (tag: string) => void;
   }
 }
