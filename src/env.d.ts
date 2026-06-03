82d4d5906434926eac7fa4841bbd4043e8a92222
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2026-06-03 22:49:58 +0200

    chore: silence oxlint triple-slash warning on generated env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
+// oxlint-disable-next-line typescript/triple-slash-reference -- Astro-generated type references
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@cloudflare/workers-types/latest" />
